ff7d8c0705a86ec77123e4ab4424c94441090473
supriyo <hisupriyooo@gmail.com>
2020-08-19 17:14:06 +0530

    Build

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,7 +26,10 @@ const pathVariants = {
 const Header = () => {
   return (
     <header>
-      <div className="logo">
+      <motion.div className="logo"
+        dragConstraints={{ left: 0, top: 0, right: 0, bottom: 0 }}
+        dragElastic={1}
+        drag>
         <motion.svg
           variants={svgVariant}
           initial="hidden"
@@ -43,7 +46,7 @@ const Header = () => {
             d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
           />
         </motion.svg>
-      </div>
+      </motion.div>
       <motion.div
         initial={{ y: -250 }}
         animate={{ y: -10 }}
