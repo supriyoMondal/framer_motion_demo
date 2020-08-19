54595c00df33e8c56ad479102e0920436f25b699
supriyo <hisupriyooo@gmail.com>
2020-08-19 17:11:44 +0530

    Updates

---
+++ precache-manifest.a256dff51c2dd0fb9ae45cd002c231f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b34d9d2e49365fcadfc769e3da6948c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fbc9c888a95045ad259",
+    "url": "/static/css/main.36ffed41.chunk.css"
+  },
+  {
+    "revision": "0cedb0ce819cd5c6acdc",
+    "url": "/static/js/2.48bb8971.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.48bb8971.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fbc9c888a95045ad259",
+    "url": "/static/js/main.62d46b29.chunk.js"
+  },
+  {
+    "revision": "9fe1e2fc4f608a1af71e",
+    "url": "/static/js/runtime-main.d6463fc5.js"
+  }
+]);
